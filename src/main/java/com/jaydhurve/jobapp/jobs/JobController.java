de9b59c959e826c1017f7e9ae24b47e4580d3cbd
jaydhurve09 <jaydhurve09@gmail.com>
2025-05-18 11:57:02 +0530

    getJobById() Service added

--- src/main/java/com/jaydhurve/jobapp/jobs/JobController.java
+++ src/main/java/com/jaydhurve/jobapp/jobs/JobController.java
@@ -1,26 +1,32 @@
 package com.jaydhurve.jobapp.jobs;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class JobController {
+    private JobService jobservice;
 
-    public List<Job> jobs = new ArrayList<>();
+    public JobController(JobService jobservice) {
+        this.jobservice = jobservice;
+    }
 
     @GetMapping("/jobs")
     public List<Job> findAll() {
-        return jobs;
+        return jobservice.findAll();
     }
 
     @PostMapping("/jobs")
     public String createJob(@RequestBody Job job) {
-        jobs.add(job);
+        jobservice.createJob(job);
         return "Job added successfully";
+    }
+
+    @GetMapping("jobs/{id}")
+    public Job getJobById(@PathVariable Long id) {
+        Job job = jobservice.getJobById(id);
+        return job;
     }
 }
